aa5c7079229494efde659a374194ffd93df2fc5a
suhrobcoder <suhrobcoder@gmail.com>
2021-11-12 10:31:34 +0500

    splash screen

--- app/src/main/java/uz/suhrob/campingapp/presentation/CampingApp.kt
+++ app/src/main/java/uz/suhrob/campingapp/presentation/CampingApp.kt
@@ -1,9 +1,11 @@
 package uz.suhrob.campingapp.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.google.accompanist.insets.ProvideWindowInsets
 import uz.suhrob.campingapp.presentation.theme.CampingAppTheme
 
@@ -11,7 +13,7 @@ import uz.suhrob.campingapp.presentation.theme.CampingAppTheme
 fun CampingApp() {
     ProvideWindowInsets {
         CampingAppTheme {
-            Surface(color = MaterialTheme.colors.background) {
+            Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
                 Text("Camping App")
             }
         }
