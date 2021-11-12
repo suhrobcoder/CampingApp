1d96b34336471a17a338fb146ef13307342f6867
suhrobcoder <suhrobcoder@gmail.com>
2021-11-12 09:43:23 +0500

    app fullscreen

--- app/src/main/java/uz/suhrob/campingapp/presentation/CampingApp.kt
+++ app/src/main/java/uz/suhrob/campingapp/presentation/CampingApp.kt
@@ -4,13 +4,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.google.accompanist.insets.ProvideWindowInsets
 import uz.suhrob.campingapp.presentation.theme.CampingAppTheme
 
 @Composable
 fun CampingApp() {
-    CampingAppTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Text("Camping App")
+    ProvideWindowInsets {
+        CampingAppTheme {
+            Surface(color = MaterialTheme.colors.background) {
+                Text("Camping App")
+            }
         }
     }
 }
--- app/src/main/java/uz/suhrob/campingapp/presentation/MainActivity.kt
+++ app/src/main/java/uz/suhrob/campingapp/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
 package uz.suhrob.campingapp.presentation
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
         setContent {
             CampingApp()
         }
